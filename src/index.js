aada3d1b3e52dc07395948fcd81372b14bc020d6
Oliver Nordbjerg <hi@notbjerg.me>
2021-06-04 00:30:58 +0200

    Remove unused Honeycomb balance proxy logic

--- src/index.js
+++ src/index.js
@@ -51,16 +51,6 @@ logger.info(`Acting as ${wallet.address}`)
 logger.info(`Connected to ${ETH_URI}`)
 logger.info(`Calling convert on ${CONTRACT_ADDRESS} every ${INTERVAL}ms`)
 
-// Token pairs to convert for
-const TOKEN_PAIRS = [
-  // HNY-WXDAI
-  ['0x71850b7e9ee3f13ab46d67167341e4bdc905eef9', '0xe91d153e0b41518a2ce8dd3d7944fa863463a97d'],
-  // HNY-WETH
-  ['0x6a023ccd1ff6f2045c3309768ead9e68f978f6e1', '0x71850b7e9ee3f13ab46d67167341e4bdc905eef9'],
-  // HNY-STAKE
-  ['0x71850b7e9ee3f13ab46d67167341e4bdc905eef9', '0xb7d311e2eb55f2f68a9440da38e7989210b9a05e']
-]
-
 async function fetchPairs (
   makerAddress
 ) {
@@ -148,47 +138,8 @@ async function convertShares (
   logger.info(`Current balance is ${balance}`)
 }
 
-// Honeycomb balance proxies to call
-const BALANCE_PROXIES = [
-  // HNY-WXDAI
-  '0x2dF0Af12DB95f16c33f496461bB2E38B1C860227',
-  // HNY-WETH
-  '0x23512464529127ec01a3422453a4416e6b569984',
-  // HNY-STAKE
-  '0x745963503a6489b91f7a8ff784d286877a775d72'
-]
-
-async function transferBalances (
-  signer,
-  proxies
-) {
-  logger.info('Transferring Honeycomb balances...')
-  for (const proxyAddress of proxies) {
-    const proxy = new ethers.Contract(
-      proxyAddress,
-      ['function transfer ()'],
-      signer
-    )
-
-    try {
-      const {
-        hash
-      } = await proxy.transfer({ gasLimit: 1400000 })
-      logger.info(`- Sent transaction to transfer balance of ${proxyAddress} pair (${hash})`)
-    } catch (err) {
-      logger.fatal(`- Transaction for ${proxyAddress} proxy failed to process.`)
-      logger.fatal(`- ${err.message}`)
-    }
-  }
-  logger.info('Done transferring balances.')
-
-  const balance = await signer.provider.getBalance(signer.address)
-  logger.info(`Current balance is ${balance}`)
-}
-
 async function main () {
   await convertShares(wallet, CONTRACT_ADDRESS)
-  await transferBalances(wallet, BALANCE_PROXIES)
 
   setTimeout(() => {
     main()
